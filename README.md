8de60dde68440e4e6e983ecd3a53735998b56735
Gabriela <krustagabriela@gmail.com>
2023-07-25 15:02:56 +0200

    library add

---
+++ README.md
@@ -0,0 +1,2 @@
+# fractol
+A project focused on creating a program that generates and visualizes various fractals using parallel processing and graphical libraries.
---
+++ test.c
@@ -0,0 +1,71 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   test.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gkrusta <gkrusta@student.42malaga.com>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/01/31 00:40:08 by W2Wizard          #+#    #+#             */
+/*   Updated: 2023/07/25 13:30:19 by gkrusta          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "MLX42/include/MLX42/MLX42.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <memory.h>
+#define WIDTH 256
+#define HEIGHT 256
+
+mlx_image_t	*g_img;
+
+void	hook(void *param)
+{
+	mlx_t	*mlx;
+
+	mlx = param;
+	if (mlx_is_key_down(param, MLX_KEY_ESCAPE))
+		mlx_close_window(param);
+	if (mlx_is_key_down(param, MLX_KEY_UP))
+		g_img->instances[0].y -= 5;
+	if (mlx_is_key_down(param, MLX_KEY_DOWN))
+		g_img->instances[0].y += 5;
+	if (mlx_is_key_down(param, MLX_KEY_LEFT))
+		g_img->instances[0].x -= 5;
+	if (mlx_is_key_down(param, MLX_KEY_RIGHT))
+		g_img->instances[0].x += 5;
+}
+
+void my_scrollhook(double xdelta, double ydelta, void* param)
+{
+	// Simple up or down detection.
+	if (ydelta > 0)
+		puts("Up!");
+	else if (ydelta < 0)
+		puts("Down!");
+	
+	// Can also detect a mousewheel that go along the X (e.g: MX Master 3)
+	if (xdelta < 0)
+		puts("Sliiiide to the left!");
+	else if (xdelta > 0)
+		puts("Sliiiide to the right!");
+/* 	fflush(stdout); // Flush the output buffer to print the messages immediatel
+ */}
+
+int32_t	main(void)
+{
+	mlx_t	*mlx;
+
+	mlx = mlx_init(WIDTH, HEIGHT, "MLX42", true);
+	if (!mlx)
+		exit(EXIT_FAILURE);
+	mlx_scroll_hook(mlx, &my_scrollhook, NULL); // for scroll
+	g_img = mlx_new_image(mlx, 128, 128);
+	memset(g_img->pixels, 255, g_img->width * g_img->height * sizeof(int));
+	mlx_image_to_window(mlx, g_img, 5, 50);
+	mlx_loop_hook(mlx, &hook, mlx);
+	mlx_loop(mlx);
+	mlx_terminate(mlx);
+	return (EXIT_SUCCESS);
+}
